eefb0865f268926d2d079fa2456341bcb85ebfc4
zl22good <zl22good@siena.edu>
2018-04-26 13:43:28 -0400

    comments needed

--- src/coffeesucks/Player.java
+++ src/coffeesucks/Player.java
@@ -18,7 +18,7 @@ public class Player implements Serializable{
     ArrayList<Meeple> myMeeps = new ArrayList<>();
     ArrayList<Ticket> myTix = new ArrayList<>();
     int ID;
-    int numTrains = 3;
+    int numTrains = 15;
     int myScore = 0;
     int numTravel = 0;
 
